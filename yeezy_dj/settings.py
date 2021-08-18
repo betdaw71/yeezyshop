3aa1751c20ed4c4be5204709a97c5c746523c9cb
David Babayan <40572719+betdaw71@users.noreply.github.com>
2021-08-18 18:15:17 +0300

    Update settings.py

--- yeezy_dj/settings.py
+++ yeezy_dj/settings.py
@@ -125,9 +125,9 @@ STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 # STATIC_ROOT = '/static/'
 
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
-)
+# STATICFILES_DIRS = (
+#     os.path.join(BASE_DIR, 'static'),
+# )
 
 import dj-database-url
 
