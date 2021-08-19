71a0db89a3bc0744cb14102fd699ee31e92e0098
David Babayan <40572719+betdaw71@users.noreply.github.com>
2021-08-19 17:59:17 +0300

    Update settings.py

--- yeezy_dj/settings.py
+++ yeezy_dj/settings.py
@@ -122,12 +122,12 @@ THOUSAND_SEPARATOR = ' '
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+# STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 # STATIC_ROOT = '/static/'
 
-# STATICFILES_DIRS = (
-#     os.path.join(BASE_DIR, 'static'),
-# )
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
 
 import dj_database_url
 
