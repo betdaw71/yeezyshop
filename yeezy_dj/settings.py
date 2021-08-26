93b30ccd38cb6a8c51c175df5c25f2dfe827b9ba
David Babayan <40572719+betdaw71@users.noreply.github.com>
2021-08-27 02:40:05 +0300

    Update settings.py

--- yeezy_dj/settings.py
+++ yeezy_dj/settings.py
@@ -120,24 +120,17 @@ THOUSAND_SEPARATOR = ' '
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
-
-STATIC_URL = '/static/'
-# STATIC_ROOT = os.path.join(BASE_DIR, 'static')
-# STATIC_ROOT = '/static/'
-
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
-)
-
 import dj_database_url
 
 db_from_env = dj_database_url.config()
-DATABASE['default'].update(db_from_env)
+DATABASES['default'].update(db_from_env)
 
+PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
 
-MEDIA_ROOT = os.path.join(BASE_DIR, "media")
-
-MEDIA_URL = "/media/"
+STATIC_ROOT = os.path.join(PROJECT_ROOT, 'static') 
+MEDIA_ROOT = os.path.join(PROJECT_ROOT, 'media')
+STATIC_URL = '/static/'
+MEDIA_URL = '/media/' 
 
 
 EMAIL_HOST = 'smtp.gmail.com'
